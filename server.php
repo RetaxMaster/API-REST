5afe2b66204d9e0609b8b2f9dc98a6ba47cb1f84
RetaxMaster <retaxmaster@hotmail.com>
2020-11-10 16:47:05 -0600

    Adicion de autenticacion con HMAC

--- server.php
+++ server.php
@@ -1,10 +1,23 @@
 <?php
 
-// PHP_AUTH_USER es la manera de PHP de codificarlo
-$user = array_key_exists( "PHP_AUTH_USER", $_SERVER ) ? $_SERVER["PHP_AUTH_USER"] : "";
-$pwd = array_key_exists( "PHP_AUTH_PW", $_SERVER ) ? $_SERVER["PHP_AUTH_PW"] : "";
+// X se usa para marcar que son headers no estándares
+if(
+    !array_key_exists("HTTP_X_HASH", $_SERVER) ||
+    !array_key_exists("HTTP_X_TIMESTAMP", $_SERVER) ||
+    !array_key_exists("HTTP_X_UID", $_SERVER)
+) die;
 
-if( $user !== "carlos" || $pwd !== "1234" ) die;
+list( $hash, $uid, $timestamp) = [
+    $_SERVER["HTTP_X_HASH"],
+    $_SERVER["HTTP_X_UID"],
+    $_SERVER["HTTP_X_TIMESTAMP"],
+];
+
+$secret = "Sh!! No se lo cuentes a nadie!";
+
+$newHash = sha1($uid.$timestamp.$secret);
+
+if ( $newHash !== $hash ) die;
 
 // Definimos los recursos disponibles
 $allowedResourceTypes = [
